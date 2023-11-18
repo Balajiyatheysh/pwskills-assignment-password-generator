0c2dc4ecae8293e9199cc93b448a02778f6a1601
balajiyatheysh <balajiyatheysh87@gmail.com>
2023-11-18 05:51:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeLowercaseElement = document.getElementById('includeLowercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+function copyFunction() {
+
+ 
+ // Get the text from the h3 element
+ const headingText = document.getElementById('passwordDisplay').innerText;
+
+ // Create a temporary textarea to hold the text
+ const tempTextarea = document.createElement('textarea');
+ tempTextarea.value = headingText;
+
+ // Append the textarea to the document (not visible)
+ document.body.appendChild(tempTextarea);
+
+ // Select the text inside the textarea
+ tempTextarea.select();
+ tempTextarea.setSelectionRange(0, 99999); // For mobile devices
+
+ // Copy the selected text to the clipboard
+ document.execCommand('copy');
+
+ // Remove the temporary textarea
+ document.body.removeChild(tempTextarea);
+
+ alert('Text copied to clipboard: ' + headingText); 
+
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+}
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeLowercase = includeLowercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols, includeLowercase)
+  passwordDisplay.innerText = password
+})
+
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
